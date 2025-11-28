7d67ff01828335a6f5f430fe2e56502fb50c13a7
michael <michael.lukyanov@student.kdg.be>
2025-11-28 14:43:11 +0100

    added requirements

--- src/train_extension.py
+++ src/train_extension.py
@@ -6,5 +6,5 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 def train_extension(cfg_path="config/config_extension.yaml"):
 
 
-# TODO extension implementation
+ # TODO extension implementation
 if __name__ == '__main__': train_extension()
